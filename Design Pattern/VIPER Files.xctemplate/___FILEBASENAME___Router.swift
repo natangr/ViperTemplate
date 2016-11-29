e2270d9b22ba16825345a9550cc6ce1d7bf2f937
Natan Grando <natan@lockitron.com>
2016-11-29 11:01:12 -0200

    Moves example to router bottom

--- Design Pattern/VIPER Files.xctemplate/___FILEBASENAME___Router.swift
+++ Design Pattern/VIPER Files.xctemplate/___FILEBASENAME___Router.swift
@@ -15,31 +15,30 @@ class ___FILEBASENAME___Router {
     // MARK: Properties
 
     weak var view: UIViewController?
-
-    /*  Example of presenting this module. Note: should be moved to the router that will present this module.
-    func present___FILEBASENAME___Screen() {
-        guard let view = ViewControllerHelper.get___FILEBASENAME___ViewController()
- else { return }
-        let presenter = ___FILEBASENAME___Presenter()
-        let router = ___FILEBASENAME___Router()
-        let interactor = ___FILEBASENAME___Interactor()
-
-        view.presenter =  presenter
-
-        presenter.view = view
-        presenter.router = router
-        presenter.interactor = interactor
-
-        router.view = view
-
-        interactor.output = presenter
-
-        self.view?.present(view, animated: true, completion: nil)
-    }
-    */
 }
 
 
 extension ___FILEBASENAME___Router: ___FILEBASENAME___Wireframe {
 
 }
+
+/*  Example of presenting this module. Note: should be moved to the router that will present this module.
+func present___FILEBASENAME___Screen() {
+    let view = ViewControllerHelper.get___FILEBASENAME___ViewController()
+    let presenter = ___FILEBASENAME___Presenter()
+    let router = ___FILEBASENAME___Router()
+    let interactor = ___FILEBASENAME___Interactor()
+
+    view.presenter =  presenter
+
+    presenter.view = view
+    presenter.router = router
+    presenter.interactor = interactor
+
+    router.view = view
+
+    interactor.output = presenter
+
+    self.view?.navigationController?.pushViewController(view, animated: true)
+}
+*/
