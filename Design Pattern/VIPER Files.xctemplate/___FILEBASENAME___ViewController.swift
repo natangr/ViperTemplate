ab21d49974ae52184558151f039c3935c522e999
Natan Grando <natan@lockitron.com>
2017-05-29 18:32:38 -0300

    Add reusable view to view controller

--- Design Pattern/VIPER Files.xctemplate/___FILEBASENAME___ViewController.swift
+++ Design Pattern/VIPER Files.xctemplate/___FILEBASENAME___ViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 
-class ___FILEBASENAME___ViewController: BaseViewController {
+class ___FILEBASENAME___ViewController: BaseViewController: ReusableView {
 
     // MARK: Static
 
